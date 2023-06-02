98a7369c289afc58a8dbb9946f969c1f7d77e5c8
Mauro Lacy <mauro@lacy.com.es>
2023-06-02 09:59:33 +0200

    Add init_app helper

--- contracts/native-staking-proxy/src/multitest.rs
+++ contracts/native-staking-proxy/src/multitest.rs
@@ -9,12 +9,7 @@ use crate::msg::ConfigResponse;
 
 const DENOM: &str = "TOKEN"; // cw-multi-test v0.16.x does not support custom tokens yet
 
-#[test]
-fn instantiation() {
-    let owner = "staking"; // The staking contract is the owner of the staking-proxy contracts
-    let user = "user1"; // One who wants to local stake (uses the proxy)
-    let validator = "validator1"; // Where to stake
-
+fn init_app(owner: &str, validator: &str) -> App {
     // Fund the staking contract, and add validator to staking keeper
     let block = mock_env().block;
     let app = MtApp::new(|router, api, storage| {
@@ -34,7 +29,16 @@ fn instantiation() {
             .add_validator(api, storage, &block, valoper1)
             .unwrap();
     });
-    let app = App::new(app);
+    App::new(app)
+}
+
+#[test]
+fn instantiation() {
+    let owner = "staking"; // The staking contract is the owner of the staking-proxy contracts
+    let user = "user1"; // One who wants to local stake (uses the proxy)
+    let validator = "validator1"; // Where to stake
+
+    let app = init_app(owner, validator);
 
     // Contract setup, with funds transfer
     let staking_proxy_code = contract::multitest_utils::CodeId::store_code(&app);
@@ -80,26 +84,7 @@ fn staking() {
     let user = "user1"; // One who wants to local stake (uses the proxy)
     let validator = "validator1"; // Where to stake
 
-    // Fund the staking contract, and add validator to staking keeper
-    let block = mock_env().block;
-    let app = MtApp::new(|router, api, storage| {
-        router
-            .bank
-            .init_balance(storage, &Addr::unchecked(owner), coins(1000, DENOM))
-            .unwrap();
-
-        let valoper1 = Validator {
-            address: validator.to_owned(),
-            commission: Decimal::percent(10),
-            max_commission: Decimal::percent(20),
-            max_change_rate: Decimal::percent(1),
-        };
-        router
-            .staking
-            .add_validator(api, storage, &block, valoper1)
-            .unwrap();
-    });
-    let app = App::new(app);
+    let app = init_app(owner, validator);
 
     let staking_proxy_code = contract::multitest_utils::CodeId::store_code(&app);
     let staking_proxy = staking_proxy_code
@@ -113,7 +98,7 @@ fn staking() {
     staking_proxy
         .stake(validator.to_owned())
         .with_funds(&coins(2, DENOM))
-        .call(owner)
+        .call(owner) // Vault has the funds
         .unwrap();
 
     // Check that new funds have been staked as well
