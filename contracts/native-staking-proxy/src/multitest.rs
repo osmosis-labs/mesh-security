f9d1b77351af96f654123d88d37094033a195677
Mauro Lacy <mauro@lacy.com.es>
2023-06-04 21:10:56 +0200

    Add withdraw rewards test

--- contracts/native-staking-proxy/src/multitest.rs
+++ contracts/native-staking-proxy/src/multitest.rs
@@ -309,3 +309,59 @@ fn releasing_unbonded() {
         coin(200, OSMO)
     );
 }
+
+#[test]
+fn withdrawing_rewards() {
+    let owner = "vault_admin";
+
+    let _vault_addr = "contract0"; // First created contract
+    let _staking_addr = "contract1"; // Second created contract. Created by vault contract on init
+    let proxy_addr = "contract2"; // Third contract (instantiated by staking contract on stake)
+
+    let user = "user1"; // One who wants to local stake (uses the proxy)
+    let validator = "validator1"; // Where to stake / unstake
+
+    let app = init_app(user, &[validator]); // Fund user, create validator
+    let vault = setup(&app, owner).unwrap();
+
+    // Bond some funds to the vault
+    vault
+        .bond()
+        .with_funds(&coins(200, OSMO))
+        .call(user)
+        .unwrap();
+
+    // Stakes some of it locally. This instantiates the staking proxy contract for user
+    vault
+        .stake_local(
+            coin(100, OSMO),
+            to_binary(&mesh_native_staking::msg::StakeMsg {
+                validator: validator.to_owned(),
+            })
+            .unwrap(),
+        )
+        .call(user)
+        .unwrap();
+
+    // Record current user funds
+    let original_funds = app.app().wrap().query_balance(user, OSMO).unwrap();
+
+    // Access staking proxy instance
+    let staking_proxy = contract::multitest_utils::NativeStakingProxyContractProxy::new(
+        Addr::unchecked(proxy_addr),
+        &app,
+    );
+
+    // Advance time enough for rewards to accrue
+    app.update_block(|block| {
+        block.height += 12345678;
+        block.time = block.time.plus_seconds(123456789);
+    });
+
+    // Withdraw rewards
+    staking_proxy.withdraw_rewards().call(user).unwrap();
+
+    // User now has some rewards
+    let current_funds = app.app().wrap().query_balance(user, OSMO).unwrap();
+    assert!(current_funds.amount > original_funds.amount);
+}
