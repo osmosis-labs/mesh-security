a2c99048d70101345e20a8c92f897f95f84da83f
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-06-05 21:24:57 +0200

    Move all provider contracts into own subdir

--- devtools/build_schemas.sh
+++ devtools/build_schemas.sh
@@ -6,11 +6,11 @@ rm -rf ./schemas
 mkdir -p ./schemas
 
 BASEDIR=$(pwd)
-for C in contracts/*/Cargo.toml contracts/consumer/*/Cargo.toml; do
+for C in contracts/{consumer,provider}/*/Cargo.toml; do
   DIR=$(dirname "$C")
   echo "Building schema for $DIR"
   (
-    cd "$DIR"; 
+    cd "$DIR";
     cargo schema > /dev/null;
     ls ./schema/*.json;
     cp ./schema/*.json "$BASEDIR/schemas";
