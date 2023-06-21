56c2270e5b042f2379be4ba3dec8f79d36984e00
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-06-21 11:28:57 +0200

    Add tests to version checking

--- packages/apis/src/ibc/version.rs
+++ packages/apis/src/ibc/version.rs
@@ -34,6 +34,13 @@ pub struct ProtocolVersion {
 }
 
 impl ProtocolVersion {
+    pub fn new(protocol: &str, version: &str) -> Self {
+        ProtocolVersion {
+            protocol: protocol.to_string(),
+            version: version.to_string(),
+        }
+    }
+
     pub fn validate(&self) -> Result<Version, VersionError> {
         if self.protocol != PROTOCOL_NAME {
             return Err(VersionError::InvalidProtocol(self.protocol.clone()));
@@ -56,12 +63,12 @@ impl ProtocolVersion {
         if proposed < min_ver {
             Err(VersionError::VersionTooOld {
                 proposed: proposed.to_string(),
-                supported: self.version.to_string(),
+                supported: min_ver.to_string(),
             })
         } else if proposed.major > supported_ver.major {
             Err(VersionError::VersionTooNew {
                 proposed: proposed.to_string(),
-                supported: self.version.to_string(),
+                supported: supported_ver.to_string(),
             })
         } else {
             let ver = std::cmp::min(proposed, supported_ver);
@@ -87,13 +94,13 @@ impl ProtocolVersion {
         if proposed < min_ver {
             Err(VersionError::VersionTooOld {
                 proposed: proposed.to_string(),
-                supported: self.version.to_string(),
+                supported: min_ver.to_string(),
             })
         } else if proposed > supported_ver {
             // we compare full version, not just major version like above
             Err(VersionError::VersionTooNew {
                 proposed: proposed.to_string(),
-                supported: self.version.to_string(),
+                supported: supported_ver.to_string(),
             })
         } else {
             Ok(())
@@ -116,8 +123,124 @@ pub fn validate_channel_order(check: &IbcOrder) -> Result<(), VersionError> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn todo_implement_tests() {
-        todo!();
+    fn validate_works() {
+        let valid = ProtocolVersion::new(PROTOCOL_NAME, "1.2.3");
+        assert_eq!(valid.validate().unwrap(), Version::new(1, 2, 3));
+        // we can only verify it validates. I cannot see how to manually construct the alpha.1 ending without parse or crate-private functions
+        let valid_alpha = ProtocolVersion::new(PROTOCOL_NAME, "1.3.2-alpha.1");
+        valid_alpha.validate().unwrap();
+
+        // invalid version numbering
+        let err = ProtocolVersion::new(PROTOCOL_NAME, "1.2.3a")
+            .validate()
+            .unwrap_err();
+        assert_eq!(err, VersionError::InvalidVersion("1.2.3a".to_string()));
+
+        // invalid protocol name
+        let err = ProtocolVersion::new("mashup-security", "1.2.3")
+            .validate()
+            .unwrap_err();
+        assert_eq!(
+            err,
+            VersionError::InvalidProtocol("mashup-security".to_string())
+        );
+    }
+
+    #[test]
+    fn to_string_works() {
+        let supported = ProtocolVersion::new(PROTOCOL_NAME, "1.2.3");
+        assert_eq!(
+            supported.to_string().unwrap(),
+            r#"{"protocol":"mesh-security","version":"1.2.3"}"#.to_string()
+        );
+    }
+
+    #[test]
+    fn build_response_works() {
+        // they propose the same version we want
+        let supported = ProtocolVersion::new(PROTOCOL_NAME, "1.2.3");
+        let response = supported.build_response("1.2.3", "1.0.2").unwrap();
+        assert_eq!(response, supported);
+
+        // they propose a newer version, but same major version
+        let a_bit_newer = ProtocolVersion::new(PROTOCOL_NAME, "1.3.0");
+        let response = a_bit_newer.build_response("1.2.3", "1.0.2").unwrap();
+        assert_eq!(response, supported);
+
+        // they propose older but supported version
+        let a_bit_older = ProtocolVersion::new(PROTOCOL_NAME, "1.0.7");
+        let response = a_bit_older.build_response("1.2.3", "1.0.2").unwrap();
+        assert_eq!(response, a_bit_older);
+
+        // they propose something with higher major version
+        let too_new = ProtocolVersion::new(PROTOCOL_NAME, "2.0.0-alpha.1");
+        let err = too_new.build_response("1.2.3", "1.0.2").unwrap_err();
+        assert_eq!(
+            err,
+            VersionError::VersionTooNew {
+                proposed: "2.0.0-alpha.1".to_string(),
+                supported: "1.2.3".to_string()
+            }
+        );
+
+        // they propose before our min supported version
+        let too_old = ProtocolVersion::new(PROTOCOL_NAME, "1.0.0");
+        let err = too_old.build_response("1.2.3", "1.0.2").unwrap_err();
+        assert_eq!(
+            err,
+            VersionError::VersionTooOld {
+                proposed: "1.0.0".to_string(),
+                supported: "1.0.2".to_string()
+            }
+        );
+    }
+
+    #[test]
+    fn verify_compatibility() {
+        // they propose the same version we want - GOOD!
+        let supported = ProtocolVersion::new(PROTOCOL_NAME, "1.2.3");
+        supported.verify_compatibility("1.2.3", "1.0.2").unwrap();
+
+        // they propose a newer version, but same major version - BAD!
+        let a_bit_newer = ProtocolVersion::new(PROTOCOL_NAME, "1.3.0");
+        let err = a_bit_newer
+            .verify_compatibility("1.2.3", "1.0.2")
+            .unwrap_err();
+        assert_eq!(
+            err,
+            VersionError::VersionTooNew {
+                proposed: "1.3.0".to_string(),
+                supported: "1.2.3".to_string()
+            }
+        );
+
+        // they propose older but supported version - GOOD
+        let a_bit_older = ProtocolVersion::new(PROTOCOL_NAME, "1.0.7");
+        a_bit_older.verify_compatibility("1.2.3", "1.0.2").unwrap();
+
+        // they propose something with higher major version - BAD
+        let too_new = ProtocolVersion::new(PROTOCOL_NAME, "2.0.0-alpha.1");
+        let err = too_new.verify_compatibility("1.2.3", "1.0.2").unwrap_err();
+        assert_eq!(
+            err,
+            VersionError::VersionTooNew {
+                proposed: "2.0.0-alpha.1".to_string(),
+                supported: "1.2.3".to_string()
+            }
+        );
+
+        // they propose before our min supported version - BAD!
+        let too_old = ProtocolVersion::new(PROTOCOL_NAME, "1.0.0");
+        let err = too_old.verify_compatibility("1.2.3", "1.0.2").unwrap_err();
+        assert_eq!(
+            err,
+            VersionError::VersionTooOld {
+                proposed: "1.0.0".to_string(),
+                supported: "1.0.2".to_string()
+            }
+        );
     }
 }
