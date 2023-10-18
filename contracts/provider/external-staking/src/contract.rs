f1f5d9a5da17cb3d56b599f5789cb9ceabf76383
Mauro Lacy <mauro@lacy.com.es>
2023-10-18 09:38:10 +0200

    Add validator update does not change state test

--- contracts/provider/external-staking/src/contract.rs
+++ contracts/provider/external-staking/src/contract.rs
@@ -2188,4 +2188,105 @@ mod tests {
             ]
         );
     }
+
+    #[test]
+    fn valset_update_updates_keep_state() {
+        let mut deps = mock_dependencies();
+        let (mut ctx, contract) = do_instantiate(deps.as_mut());
+
+        // We add two new validators, and tombstone one
+        let adds = vec![
+            AddValidator {
+                valoper: "alice".to_string(),
+                pub_key: "alice_pub_key".to_string(),
+            },
+            AddValidator {
+                valoper: "bob".to_string(),
+                pub_key: "bob_pub_key".to_string(),
+            },
+        ];
+
+        contract
+            .valset_update(
+                ctx.deps.branch(),
+                ctx.env.clone(),
+                100,
+                1234,
+                &adds,
+                &[],
+                &[],
+                &[],
+                &[],
+                &[],
+            )
+            .unwrap();
+
+        // Bob is removed next
+        let update_ctx = ctx.branch();
+        let rems = vec!["bob".to_string()];
+        let (evt, _msgs) = contract
+            .valset_update(
+                update_ctx.deps,
+                update_ctx.env,
+                200,
+                2345,
+                &[],
+                &rems,
+                &[],
+                &[],
+                &[],
+                &[],
+            )
+            .unwrap();
+
+        // Check the event
+        assert_eq!(evt.attributes, vec![Attribute::new("removals", "bob"),]);
+
+        // Bob is updated next
+        let update_ctx = ctx.branch();
+        let upds = vec![AddValidator {
+            valoper: "bob".to_string(),
+            pub_key: "bob_pub_key_updated".to_string(),
+            }
+        ];
+        let (evt, _msgs) = contract
+            .valset_update(
+                update_ctx.deps,
+                update_ctx.env,
+                200,
+                2345,
+                &[],
+                &[],
+                &upds,
+                &[],
+                &[],
+                &[],
+            )
+            .unwrap();
+
+        // Check the event
+        assert_eq!(evt.attributes, vec![Attribute::new("updated", "bob"),]);
+
+        let query_deps = ctx.deps;
+        let query_ctx = QueryCtx {
+            deps: query_deps.as_ref(),
+            env: mock_env(),
+        };
+
+        // Check that bob is still unbonded
+        let vals = contract.list_validators(query_ctx, None, None).unwrap();
+        assert_eq!(
+            vals.validators,
+            vec![
+                ValidatorState {
+                    validator: "alice".to_string(),
+                    state: State::Active {}
+                },
+                ValidatorState {
+                    validator: "bob".to_string(),
+                    state: State::Unbonded {}
+                },
+            ]
+        );
+    }
 }
